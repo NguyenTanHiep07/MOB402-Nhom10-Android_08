b349d2a53b43e0b290ba2faf05e32a9280354299
Thinh Nguyen <nquocthinh06@gmail.com>
2026-09-10 22:30:15 +0700

    Thịnh: Gộp toàn bộ source code Android vào thư mục Code/Frontend

--- Code/app/src/main/java/com/mob10/deliveryapp/data/remote/dto/AdminDriverRegistrationResponseDto.kt
+++
@@ -1,9 +0,0 @@
-package com.mob10.deliveryapp.data.remote.dto
-
-data class AdminDriverRegistrationResponseDto(
-    val id: Long,
-    val user: AdminUserResponseDto?,
-    val licensePlate: String,
-    val status: String,
-    val createdAt: String
-)
--- Code/app/src/main/java/com/mob10/deliveryapp/data/remote/dto/RegisterRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.mob10.deliveryapp.data.remote.dto
-
-data class RegisterRequest(
-    val username: String,
-    val password: String,
-    val fullName: String,
-    val phoneNumber: String
-)
